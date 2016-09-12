eb081ddf47c8910ec213aafbf85640ba5e9e63f1
CuiYuanlong <long_800110@163.com>
2016-09-12 17:36:07 +0800

    test

---
+++ Application/Wechat/Conf/config_sae.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'WECHAT_OPTIONS' => array(
+		'token' => 'cuiyuanlongweixin',
+		'appid' => 'wx2389537b7e6f66c9',
+		'appsecret' => 'd313c34b1061d4acee6d8e048f6e8921',
+	),
+	
+);
